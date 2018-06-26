a53645f68f99e1e959dfeede68bbbbe7c78f1ac9
brian-cai <briancai@gatech.edu>
2018-06-25 21:22:11 -0400

    removed some berkeley pacman comments

--- shapeshifter.py
+++ shapeshifter.py
@@ -1,47 +1,8 @@
-# coding=utf-8
-
-
-# searchAgents.py
-# ---------------
-# Licensing Information:  You are free to use or extend these projects for 
-# educational purposes provided that (1) you do not distribute or publish 
-# solutions, (2) you retain this notice, and (3) you provide clear 
-# attribution to UC Berkeley, including a link to 
+# Original Code: Pacman search code from
 # http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero 
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and 
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-
-"""
-This file contains all of the agents that can be selected to
-control Pacman.  To select an agent, use the '-p' option
-when running pacman.py.  Arguments can be passed to your agent
-using '-a'.  For example, to load a SearchAgent that uses
-depth first search (dfs), run the following command:
-
-> python pacman.py -p SearchAgent -a fn=depthFirstSearch
-
-Commands to invoke other search strategies can be found in the
-project description.
-
-Please only change the parts of the file you are asked to.
-Look for the lines that say
-
-"*** YOUR CODE HERE ***"
-
-The parts you fill in start about 3/4 of the way down.  Follow the
-project description for details.
-
-Good luck and happy searching!
-"""
 
 import search
-
-
 
 
 class ShapeShifterSearchProblem(search.SearchProblem):
@@ -157,7 +118,7 @@ def shapeshifterHeuristic(state, problem):
 #    magic = (rotationsum) % (3) - len(piecesleft)
     magic = (rotationsum) - len(piecesleft)
 
-    
+
     #return (magic);
 
     htotal = 0
@@ -286,7 +247,7 @@ if __name__ == "__main__":
               ((3, 3), ((0, 1, 0, 0), (1, 1, 1, 0), (0, 1, 0, 0), (0, 0, 0, 0)))
     )
     gamemap = ((2, 1, 0, 0),(1, 0, 1, 0),(2, 2, 0, 0),(0, 0, 0, 0))
-    # path = [(0, 1), (0, 1), (0, 0), (0, 0), (0, 0), (0, 1), (0, 1), (0, 1), (0, 0), (1, 0), (0, 0)]
+    path = [(0, 1), (0, 1), (0, 0), (0, 0), (0, 0), (0, 1), (0, 1), (0, 1), (0, 0), (1, 0), (0, 0)]
     startState = (pieces, gamemap)
     print('__main__: startState =',startState)
     problem = ShapeShifterSearchProblem(startState, numranks=3, cycle=[2,1,0]) # speed does NOT seem to be a py3 vs py2 problem
