7f3639c95a6c4670314d4ff17f549d447f606a84
brian-cai <briancai@gatech.edu>
2018-07-20 16:24:46 -0400

    restructured 1

--- shapeshifter_html.py
+++ shapeshifter_html.py
@@ -1,6 +1,38 @@
 from bs4 import BeautifulSoup
-import requests
 import re
+
+# pieces information (Counter)
+pieces_to_ints = dict()
+
+
+def get_shapeshifter_config(filename):
+    create_dict()
+
+    # HTML LEVEL GOES HERE:
+    with open(filename, encoding="utf8") as f:
+        read_data = f.read()
+    soup = BeautifulSoup(read_data, 'html.parser')
+
+    cycle = update_dict_and_get_cycle(pieces_to_ints, soup)
+
+    board = fetch_board_state(soup)
+
+    BOARD_DIMENSIONS = 4
+    final_pieces = []
+
+    add_active_piece(soup, final_pieces, BOARD_DIMENSIONS)
+
+    add_other_shape_pieces(soup, final_pieces, BOARD_DIMENSIONS)
+
+    return board, final_pieces, cycle
+
+
+def create_dict():
+    pieces_to_ints['gob_0'] = 0
+    pieces_to_ints['gau_0'] = 0
+    pieces_to_ints['swo_0'] = 0
+    pieces_to_ints['cro_0'] = 0
+    return pieces_to_ints
 
 
 def update_dict_and_get_cycle(pieces_to_ints, soup):
@@ -25,44 +57,33 @@ def update_dict_and_get_cycle(pieces_to_ints, soup):
     cycle = [pieces_to_ints[cycle_images[i]] for i in range(len(cycle_images))]
     # print(cycle)
     return cycle
-def get_shapeshifter_config(filename):
 
-    pieces_to_ints = dict()
-    pieces_to_ints['gob_0'] = 0
-    pieces_to_ints['gau_0'] = 0
-    pieces_to_ints['swo_0'] = 0
-    pieces_to_ints['cro_0'] = 0
 
-    #HTML LEVEL GOES HERE:
-    with open(filename, encoding="utf8") as f:
-        read_data = f.read()
-    soup = BeautifulSoup(read_data, 'html.parser')
-
-    cycle = update_dict_and_get_cycle(pieces_to_ints, soup);
-
+def fetch_board_state(soup):
     board_html = soup.select('#content > table > tbody > tr > td.content > table > tbody tr')
 
     board = []
 
     for row in board_html:
         img_indices = [m.start() for m in re.finditer('.gif', str(row))]
-        board.append(tuple(pieces_to_ints[str(row)[idx-5:idx]] for idx in img_indices))
+        board.append(tuple(pieces_to_ints[str(row)[idx - 5:idx]] for idx in img_indices))
 
     board = tuple(board)
 
-    #need to get ACTIVE SHAPE separately
 
-    BOARD_DIMENSIONS = 4
-    final_pieces = []
+    return board
 
+
+def add_active_piece(soup, final_pieces, BOARD_DIMENSIONS):
     #  need to get ACTIVE SHAPE separately
-    active_shape_rows = soup.select('#content > table > tbody > tr > td.content > center:nth-of-type(3) > table > tbody > tr > td > table > tbody > tr')
+    active_shape_rows = soup.select(
+        '#content > table > tbody > tr > td.content > center:nth-of-type(3) > table > tbody > tr > td > table > tbody > tr')
 
     cur_piece = []
     cp_dim = [0, 0]
     for pr in active_shape_rows:
         cur_row = []
-        td_list = BeautifulSoup(str(pr),'html.parser').findAll('td', recursive=True)
+        td_list = BeautifulSoup(str(pr), 'html.parser').findAll('td', recursive=True)
         for td in td_list:
             if 'img' in str(td):
                 cur_row.append(1)
@@ -75,21 +96,18 @@ def get_shapeshifter_config(filename):
     cur_piece.extend([(0,) * BOARD_DIMENSIONS for _ in range(BOARD_DIMENSIONS - len(cur_piece))])  # add empty row(s)
     final_pieces.append(tuple((cp_dim, cur_piece)))
 
-    # print()
-    # print('active_shape_rows =', active_shape_rows)
-    # print()
 
-
-    shapes = soup.select('#content > table > tbody > tr > td.content > center:nth-of-type(3) > center > table > tbody > tr > td')
-
+def add_other_shape_pieces(soup, final_pieces, BOARD_DIMENSIONS):
+    shapes = soup.select(
+        '#content > table > tbody > tr > td.content > center:nth-of-type(3) > center > table > tbody > tr > td')
 
     for shape in shapes:
         cur_piece = []
         cp_dim = [0, 0]
-        piece_rows = BeautifulSoup(str(shape),'html.parser').findAll('tr', recursive=True)
+        piece_rows = BeautifulSoup(str(shape), 'html.parser').findAll('tr', recursive=True)
         for pr in piece_rows:
             cur_row = []
-            td_list = BeautifulSoup(str(pr),'html.parser').findAll('td', recursive=True)
+            td_list = BeautifulSoup(str(pr), 'html.parser').findAll('td', recursive=True)
             for td in td_list:
                 if 'img' in str(td):
                     cur_row.append(1)
@@ -97,46 +115,39 @@ def get_shapeshifter_config(filename):
                     cur_row.append(0)
             cp_dim[0] = max(cp_dim[0], len(cur_row))
 
-            #piece dimensions
-            cur_row.extend((0,)*(BOARD_DIMENSIONS - len(cur_row)))
+            # piece dimensions
+            cur_row.extend((0,) * (BOARD_DIMENSIONS - len(cur_row)))
             cur_piece.append(tuple(cur_row))
         cp_dim[1] = max(cp_dim[1], len(cur_piece))
 
         # add empty row(s)
-        cur_piece.extend([(0,)*BOARD_DIMENSIONS for _ in range(BOARD_DIMENSIONS - len(cur_piece))])
+        cur_piece.extend([(0,) * BOARD_DIMENSIONS for _ in range(BOARD_DIMENSIONS - len(cur_piece))])
 
         final_pieces.append(tuple((cp_dim, cur_piece)))
 
     final_pieces = tuple(tuple(tuple(row) for row in piece) for piece in final_pieces)
 
 
-    return board, final_pieces, cycle
-
-
 def test_shapeshifter_html(filename):
-    board, pieces, cycle = get_shapeshifter_config(filename);
+    board, pieces, cycle = get_shapeshifter_config(filename)
 
     #Print Board
     print("Board: ")
     for row in board:
         print(row)
-    print("Original, ", board)
     print()
 
     #Gets Pieces
     count = 1
-    for sizenxn,board in pieces:
-        print("size: ", sizenxn)
-        print("piece ", count, ":")
+    for sizenxn, board in pieces:
+        print("piece ", count, " size: ", sizenxn)
         for row in board:
             print(row)
         print()
         count = count + 1
 
-    print("Original, ", pieces)
-    print()
     print("Cycle: ", cycle)
 
-#uncomment to test this file
+# uncomment to test this file
 test_shapeshifter_html('htmllevels/defaultlevel.txt')
-#get_shapeshifter_config('htmllevels/level1.txt')
+# get_shapeshifter_config('htmllevels/level1.txt')
